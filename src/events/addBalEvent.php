a894119d17bf396cda56219f39156bdbf648c946
iNA16 <134897452+iNA16@users.noreply.github.com>
2023-07-04 20:23:12 +0300

    Update addBalEvent.php

--- src/events/addBalEvent.php
+++ src/events/addBalEvent.php
@@ -9,11 +9,18 @@ use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use iNA16\EconomyBridge\events\userExistsEvent;
 
 class addBalEvent extends PluginBase{
   public $db = $this->getDataFolder() . "players_data.yml";
   
-  public function addBal(string $player) {
-    # codes using $db such as 
+  public function addBal(string $player, int $amount) {
+    if ($this->userExists($player)) {
+      $this->updateBalance($player, $amount);
+      echo "Balance updated successfully.";
+    } else {
+      $this->newAccount($player);
+      echo "New account created successfully.";
+    }
   }
 }
